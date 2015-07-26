9ed7418ed25a8a387c08d8cfb31c19d01170ef25
esplo <esplo@users.noreply.github.com>
2015-07-26 18:39:36 +0900

    [change] now this system writes -1 to the 'number' column if we cannot get the information of the number of days
    In SwapInfo, save it as None

--- SwapInfo.scala
+++ SwapInfo.scala
@@ -7,13 +7,13 @@ import java.util.Calendar
 class SwapInfo(
                 val pair: CurrencyPair,
                 val date: Calendar,
-                val numberOfDate: Int,
+                val numberOfDate: Option[Int],  // None means that there is no information about the number of days
                 val buy: Price,
                 val sell: Price
                 ) {
 
   override def toString = {
     val format2 = DateFormat.getDateInstance
-    s"$pair ${format2.format(date.getTime)} buy:$buy sell:$sell num:$numberOfDate"
+    s"$pair ${format2.format(date.getTime)} buy:$buy sell:$sell num:${numberOfDate.getOrElse(-1)}"
   }
 }
