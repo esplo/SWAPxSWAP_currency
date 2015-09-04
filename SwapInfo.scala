ea8bc65af920d213373b97660b96d0264e5b8db1
esplo <esplo@users.noreply.github.com>
2015-09-05 01:50:43 +0900

    [change] member names in SwapInfo becomes more simple
    brokerName -> broker
    numberOfDays -> num

--- SwapInfo.scala
+++ SwapInfo.scala
@@ -5,10 +5,10 @@ import java.util.{Date, Calendar}
 
 
 case class SwapInfo(
-                     brokerName: String,
+                     broker: String,
                      pair: CurrencyPair,
                      date: Calendar,
-                     numberOfDays: Option[Int], // None means that there is no information about the number of days
+                     num: Option[Int], // None means that there is no information about the number of days
                      buy: Price,
                      sell: Price
                      ) {
@@ -16,10 +16,10 @@ case class SwapInfo(
   // convert to DB class
   def serialize: SwapInfoForDB = {
     SwapInfoForDB(
-      brokerName,
+      broker,
       pair.toString,
       date.getTime,
-      numberOfDays,
+      num,
       buy.value,
       sell.value,
       buy.currency.name
@@ -28,32 +28,32 @@ case class SwapInfo(
 
   override def toString = {
     val format2 = DateFormat.getDateInstance
-    s"broker: $brokerName $pair ${format2.format(date.getTime)} buy:$buy sell:$sell num:$numberOfDays"
+    s"broker: $broker $pair ${format2.format(date.getTime)} buy:$buy sell:$sell num:$num"
   }
 }
 
 
 // swap-info class to write DB
 case class SwapInfoForDB(
-                          brokerName: String,
+                          broker: String,
                           pair: String,
                           date: Date,
-                          numberOfDays: Option[Int],
+                          num: Option[Int],
                           buy: Long,
                           sell: Long,
-                          swapCurrency: String
+                          currency: String
                           ) {
 
   // convert to normal class
   def deserialize: SwapInfo = {
-    val currency = CurrencyFormatter.str2Currency(swapCurrency).get
+    val swapCurrency = CurrencyFormatter.str2Currency(currency).get
     SwapInfo(
-      brokerName,
+      broker,
       CurrencyFormatter.str2CurrencyPair(pair).get,
       { val c = Calendar.getInstance(); c.setTime(date); c },
-      numberOfDays,
-      new Price(currency, buy),
-      new Price(currency, sell)
+      num,
+      new Price(swapCurrency, buy),
+      new Price(swapCurrency, sell)
     )
   }
 }
